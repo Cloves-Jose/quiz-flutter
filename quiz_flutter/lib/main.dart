38dec60f89ee1857eef760436e11401574afc4fb
Cloves José <clovesjsilva8@gmail.com>
2021-08-17 23:38:34 -0300

    Stateful Widget

--- quiz_flutter/lib/main.dart
+++ quiz_flutter/lib/main.dart
@@ -15,28 +15,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class App extends StatelessWidget {
-  final List<String> perguntas = [
-    'Qual é a sua cor favorita',
-    'Qual é o seu animal favorito',
-  ];
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-          centerTitle: true,
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text('Perguntas'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
           children: <Widget>[
-            Center(
-              child: Text(perguntas.elementAt(0)),
-            ),
-            ElevatedButton(onPressed: null, child: Text('Resposta 1')),
-            ElevatedButton(onPressed: null, child: Text('Resposta 2')),
-            ElevatedButton(onPressed: null, child: Text('Resposta 3')),
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 2')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 3'))
           ],
           mainAxisAlignment: MainAxisAlignment.center,
-        ));
+        ),
+      ),
+    );
   }
 }
